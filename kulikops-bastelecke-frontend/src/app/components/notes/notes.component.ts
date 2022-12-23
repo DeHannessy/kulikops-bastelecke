0e4c839625b8b0a2b87cbde072df945ff9bad1cf
Hannes Michel Lüthi <hannes.luethi@bluewin.ch>
2022-12-23 16:46:38 +0100

    a bit of responsiveness with the menu, lets gooooo

--- kulikops-bastelecke-frontend/src/app/components/notes/notes.component.ts
+++ kulikops-bastelecke-frontend/src/app/components/notes/notes.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notes.component.css']
 })
 export class NotesComponent implements OnInit {
-  public notes = [["hi", "moin alter"], ["asödkfjaösdkjföalsjdkf", "asöldkfjasldf sldfjk asödflkj asdflkja sdölfja slökfjas lfasödfjaöjsdkfö alskdfj aöfjsdk öalfjdk asöldkfjlaöjsdkflöajsdkflö askfdlö asdf aslfdjöaljskfdlöasjdf aslfdkj aöslkjfalkfjdöalskjfd sldfjls kfdj slkfjslkfdjlskfls lfjlsdfj lskjf lskfjl sdkfjklsdfls jdflskf "], ["ljsdkfls", "alsdkföasdjf"], ["this is going to be great.", "is it tho?"], ["yep", "definitely!"]];
+  public notes = [["Kulikops Bastelecke Todos", "Notes\nI want to be able to make notes about anything, and add them into a list. in a topic. a topic has sub topics and sub topics and sub sub sub. you can show all notes or just certain ones.\nOthers:\nmake a Console, like on monkeytype, accessable by maybe typing slash\nMake color themes through the whole page, like in monkeytype.com"],["hi", "moin alter"], ["asödkfjaösdkjföalsjdkf", "asöldkfjasldf sldfjk asödflkj asdflkja sdölfja slökfjas lfasödfjaöjsdkfö alskdfj aöfjsdk öalfjdk asöldkfjlaöjsdkflöajsdkflö askfdlö asdf aslfdjöaljskfdlöasjdf aslfdkj aöslkjfalkfjdöalskjfd sldfjls kfdj slkfjslkfdjlskfls lfjlsdfj lskjf lskfjl sdkfjklsdfls jdflskf "], ["ljsdkfls", "alsdkföasdjf"], ["this is going to be great.", "is it tho?"], ["yep", "definitely!"]];
   public newNote = "";
   public newNoteTitle = "";
   public searchQuery = "";
@@ -19,11 +19,12 @@ export class NotesComponent implements OnInit {
   addNewNote() {
     if (!this.newNoteTitle) {
       this.newNoteTitle = this.newNote;
-      this.notes.push([this.newNoteTitle, ""]);
+      this.notes.unshift([this.newNoteTitle, ""]);
       document.getElementById('new-note-input')?.setAttribute("placeholder", "add content");
     } else {
-      this.notes[this.notes.length - 1][1] = this.newNote;
+      this.notes[0][1] = this.newNote;
       document.getElementById('new-note-input')?.setAttribute("placeholder", "new note title");
+      this.newNoteTitle = "";
     }
     this.newNote = "";
   }
